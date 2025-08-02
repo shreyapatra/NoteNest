107520e0e85cfa0b2011ff5bf1951d2c93b758db
Shreya Patra <shreyapatra001@gmail.com>
2025-08-02 16:50:11 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import notesRoutes from "./routes/notesRoute.js";
+import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
+
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5001;
+
+//middleware
+app.use(cors({
+    origin: "http://localhost:5173",
+}));
+app.use(express.json());
+app.use(rateLimiter);
+
+app.use("/api/notes", notesRoutes);
+
+connectDB().then(() =>{
+    app.listen(PORT, () => {
+       console.log("Server started on PORT:",PORT);
+    });
+});
+
+
